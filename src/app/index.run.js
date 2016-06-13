b2cc50a5be63637a96c8dc2d41a885a661bd29cf
xiancong.lxc <xiancong.lxc@alibaba-inc.com>
2016-06-13 09:32:41 +0800

    fix(创建): 不一定所有数据都要填写

--- src/app/index.run.js
+++ src/app/index.run.js
@@ -2,4 +2,5 @@ export function runBlock ($log, Restangular) {
   'ngInject';
   $log.debug('runBlock end');
   Restangular.setBaseUrl('http://120.26.125.197:8081');
+  // Restangular.setBaseUrl('http://localhost:8081');
 }
